df8bd8fb220ac296c4ac1de1c6cc385ea6d4f43a
mldesignerjs <mldesignerjs@gmail.com>
2023-07-05 08:24:22 +0700

    third-commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const [text, setText] = useState('Your name')
     const [fontSize, setFontSize] = useState('36')
     const [umbrellaSize, setUmbrellaSize] = useState('30')
-    const [kindWood, setKindWood] = useState('CN')
+    const [kindWood, setKindWood] = useState('cn')
     function handleChangeContent(text: string) {
         setText(text)
     }
@@ -75,43 +75,49 @@ export default function Home() {
                     <select
                         className="py-2 px-4 rounded"
                         id="floatingSelect"
-                        defaultValue="CN"
+                        defaultValue="cn"
                         onChange={(e) => handleChangeKindWood(e.target.value)}
                     >
-                        <option value="CN">CN</option>
-                        <option value="TM">Trắc/Mun</option>
+                        <option value="cn">CN</option>
+                        <option value="tm">Trắc/Mun</option>
                     </select>
                 </div>
             </div>
             <div className="flex w-full flex-wrap flex-col mt-6">
-                      {
-                        engraveFonts.map((font, key) => (
-                            <div
-                                key={key}
-                                style={font.style}
-                                className="w-auto flex items-center justify-center pr-6 pb-4"
+                {engraveFonts.map((font, key) => (
+                    <div key={key} style={font.style} className="w-auto flex items-center justify-center pr-6 pb-4">
+                        <span className="flex items-center justify-center text-xl pr-6">{key + 1}</span>
+                        <div
+                            className="flex items-center w-full overflow-hidden text-center leading-normal h-[7cm]"
+                            style={{
+                                backgroundImage: `url(${
+                                    umbrellaSize === '30' && kindWood === 'cn'
+                                        ? 'can-cn'
+                                        : umbrellaSize === '30' && kindWood === 'tm'
+                                        ? 'g30-trac'
+                                        : umbrellaSize === '27' && kindWood === 'cn'
+                                        ? 'g27-cn'
+                                        : umbrellaSize === '27' && kindWood === 'tm'
+                                        ? 'g27-trac'
+                                        : 'chagoi-den'
+                                }.jpg)`,
+                                backgroundSize: 'auto 100%',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                            <span
+                                className={`engrave flex items-center justify-center whitespace-nowrap overflow-hidden text-center leading-normal ml-[3cm] h-[15mm] ${
+                                    umbrellaSize === '23' ? 'w-[8cm]' : 'w-[12cm]'
+                                } ${kindWood === 'cn' ? 'cn' : 'tm'}`}
+                                style={{
+                                    fontSize: `${fontSize}px`,
+                                }}
                             >
-                                <span className="flex items-center justify-center text-xl pr-6">{key + 1}</span>
-                                <div
-                                    className={`flex items-center w-full overflow-hidden text-center leading-normal ${umbrellaSize === '30' ? 'h-[8cm]': 'h-[6cm]'}`}
-                                    style={{
-                                        backgroundImage: `url(${umbrellaSize === '30' ? 'can-cn' : 'chagoi-den'}.jpg)`,
-                                        backgroundSize: 'auto 100%',
-                                        backgroundRepeat: 'no-repeat',
-                                    }}
-                                >
-                                    <span
-                                        className={`engrave flex items-center justify-center whitespace-nowrap overflow-hidden text-center leading-normal ml-[3cm] h-[15mm] ${umbrellaSize === '30' ? 'w-[12cm] cn' : 'w-[8cm] tm'}`}
-                                        style={{
-                                            fontSize: `${fontSize}px`,
-                                        }}
-                                    >
-                                        {text}
-                                    </span>
-                                </div>
-                            </div>
-                        ))
-                      }
+                                {text}
+                            </span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     )
