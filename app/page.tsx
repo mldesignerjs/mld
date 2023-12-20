19b13dd48019d314a5b10f6a38c6bc5c8ece5707
mldesignerjs <98085208+mldesignerjs@users.noreply.github.com>
2023-12-20 20:42:34 +0700

    nguyenam

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+'use client'
+import { phuam } from '@/constants'
+import { useState } from 'react'
+
 export default function Home() {
-    return <div className="p-24">Home Page</div>
+    const [nguyenAm, setNguyenAm] = useState<string>('')
+    function handleNhapNguyenAm(value: string) {
+        setNguyenAm(value)
+    }
+    return (
+        <div className="px-24 pt-40">
+            <input
+                type="text"
+                placeholder="Nhập text"
+                className="p-5 border-2"
+                onChange={(e) => handleNhapNguyenAm(e.target.value)}
+            />
+            <ul className="flex flex-wrap gap-5">
+                {phuam.map((a) => (
+                    <li key={a} className="text-3xl w-36">
+                        {a}
+                        {nguyenAm}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
